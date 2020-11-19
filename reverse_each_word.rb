1754f5b4b3f4506532f225e496c67af023860004
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-19 23:42:32 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,26 @@
+# string = "Hello there and how are you?"
+# string_array = ["Hello", "there", "and", "how", "are", "you", "?"]
+
+def reverse_each_word(string)
+  string_array = []
+  string_array << string.split()
+  
+  reverse_array = []
+  string_array.each { 
+    |word|
+    reverse_array << word.reverse
+  }
+  reverse_array.join(" ")
+end
+
+
+
+def reverse_each_word(string)
+  string_array = []
+  string_array << string.split()
+  
+  string_array.collect {
+    |word|
+    word.reverse
+  }
+end
