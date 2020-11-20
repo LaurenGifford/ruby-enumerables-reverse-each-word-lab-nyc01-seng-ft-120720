822f10d440c70db90aa05db25ce37a6eafd0c189
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-20 00:23:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,10 +4,7 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
   
-  reverse_array = []
-  string_array.each { 
-    |word|
-    reverse_array << word.reverse
+  reverse_array = string_array.each {|word| word.reverse
   }
   reverse_array.join(" ")
 end
