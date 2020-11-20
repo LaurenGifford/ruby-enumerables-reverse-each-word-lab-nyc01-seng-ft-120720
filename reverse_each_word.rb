672ee7cf58815ab0e764f8bd80a8c15a5583e216
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-20 00:28:27 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+# string = "Hello there and how are you?"
+# string_array = ["Hello", "there", "and", "how", "are", "you", "?"]
+
+def reverse_each_word(string)
+  string_array = string.split(" ")
+  reverse_array = string_array.each {|word| word.reverse!}
+  return reverse_array.join(" ")
+end
+
+
+
+def reverse_each_word(string)
+  string_array = string.split(" ")
+  string_array.collect {|word| word.reverse!}
+  return string_array.join(" ")
+end
+
